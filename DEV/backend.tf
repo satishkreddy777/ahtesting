3234758953ad5d9f394e5d6c8670189d36ac8378
satishkreddy777 <satishkreddy777@gmail.com>
2025-05-16 22:51:19 +0530

    Update backend.tf

--- DEV/backend.tf
+++ DEV/backend.tf
@@ -1,1 +1,6 @@
-
+backend "azurerm" {
+      resource_group_name  = "ahtestingrg"
+      storage_account_name = "ahtestingsg"
+      container_name       = "terraformcont"
+      key                  = "terraform.tfstate"
+  }
