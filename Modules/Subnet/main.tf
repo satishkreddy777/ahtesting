91d654061059588888e38d29bcd00e14c8a9aad7
satishkreddy777 <satishkreddy777@gmail.com>
2025-05-17 21:31:12 +0530

    Update main.tf

--- Modules/Subnet/main.tf
+++ Modules/Subnet/main.tf
@@ -1,1 +1,15 @@
 
+resource "azurerm_subnet" "terrsubnet" {
+  name                 =  var.subnetname
+  resource_group_name  = var.rgsubname
+  virtual_network_name = var.vnetname
+  address_prefix       = var.subaddress_prefix
+
+  delegation {
+    name = "acctestdelegation"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
