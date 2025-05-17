252ea5cca407b8b94bf489624b3e051909a0bfe7
terraformtesting <terrformtesting447@gmail.com>
2025-05-17 10:51:01 +0530

    updated backend file

--- TEST/backend.tf
+++ TEST/backend.tf
@@ -1,1 +1,8 @@
-
+terraform {
+ backend "azurerm" {
+      resource_group_name  = "ahtestrg"
+      storage_account_name = "ahtestsg"
+      container_name       = "ahtest1cont"
+      key                  = "terraform.tfstate"
+  }
+}
