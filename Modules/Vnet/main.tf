157a32e3702a0f9ea5a8dee50f377f35b513dd07
satishkreddy777 <satishkreddy777@gmail.com>
2025-05-17 15:43:24 +0530

    Update main.tf

--- Modules/Vnet/main.tf
+++ Modules/Vnet/main.tf
@@ -1,1 +1,8 @@
 
+
+resource "azurerm_virtual_network" "example" {
+  name                = var.vnetname
+  location            = var.vnetlocation
+  resource_group_name = var.rgname
+  address_space       = var.addspace
+}
