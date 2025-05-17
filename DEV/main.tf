074ad12a6a2b91004afcc4c133ff9507e155034c
terraformtesting <terrformtesting447@gmail.com>
2025-05-17 12:42:58 +0530

    added main.tf

--- DEV/main.tf
+++ DEV/main.tf
@@ -3,3 +3,9 @@ resource "azurerm_resource_group" "firstrg" {
   name     = "testingfirstrg"
   location = "East US"
 }
+
+module "rgmodule" {
+  source = "../Modules/Resources"
+  rgname = var.rgname
+  location = var.location
+}
